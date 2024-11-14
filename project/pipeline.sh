ef3f27f4698745b5339f750f3f06a7d6810f78c7
akaishimichelle <michelle.akaishi@gmail.com>
2024-11-14 14:14:34 +0100

    Update code

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -6,5 +6,12 @@
 #   3. Make a kaggle folder in your home directory (C:\Users\{your_username}\.kaggle).
 #      You may use this command to check your home directory in command prompt: echo %USERPROFILE%
 #   4. Place the "kaggle.json" file in the folder you created in step3
+#   5. Please install the following imports before running the pipeline:
+#        import requests
+#        import sqlite3
+#        import pandas as pd
+#        import os
+#        import zipfile
+#        import kagglehub
 
 python3 ./project/pipeline.py
