7830acc5b405b1570f9296bb9ef7b9d74c2b4c75
akaishimichelle <michelle.akaishi@gmail.com>
2024-12-12 01:24:24 +0100

    pipeline.sh updated

--- project/pipeline.sh
+++ project/pipeline.sh
@@ -1,27 +1,25 @@
-#!/bin/bash
-
-#!/bin/bash
-
-# Get the directory of the script (the pipeline.sh file)
-SCRIPT_DIR=$(dirname "$(realpath "$0")")
-
-# Set the path to the tests.py file (inside the project folder)
-PIPELINE_PY="$SCRIPT_DIR/pipeline.py"
-
-# Run the Python test script
-python3 "$PIPELINE_PY"
-
-#As a pre-requisite, 
+# As a pre-requisite, 
 #   1. Please make sure to be signed in to kaggle
 #   2. Go to kaggle settings and create a new token (under API section). This will generate a "kaggle.json" file
 #   3. Make a kaggle folder in your home directory (C:\Users\{your_username}\.kaggle).
 #      You may use this command to check your home directory in command prompt: echo %USERPROFILE%
 #   4. Place the "kaggle.json" file in the folder you created in step3
 
-#   5. Please install the following imports before running the pipeline:
-#        import requests
-#        import sqlite3
-#        import pandas as pd
-#        import os
-#        import zipfile
-#        import kagglehub
+# The following imports will be automatically installed before running the pipeline via 'requirements.txt' file:
+#  kagglehub
+#  pandas 
+#  requests
+
+# Create virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+SCRIPT_DIR=$(dirname "$(realpath "$0")")
+
+REQUIREMENTS_FILE="$SCRIPT_DIR/requirements.txt"
+pip install -r "$REQUIREMENTS_FILE"
+
+ PIPELINE_PY="$SCRIPT_DIR/pipeline.py"
+ python3 "$PIPELINE_PY"
